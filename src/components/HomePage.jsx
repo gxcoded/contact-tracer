69124900602ed8cf285bee68c1521fb6fd38ecb8
gilgucela <82348697+gilgucela@users.noreply.github.com>
2022-10-29 22:02:53 +0800

    sample

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
 import "../assets/css/HomePage.css";
+import { useState } from "react";
 
 const HomePage = () => {
+  const [url] = useState(process.env.REACT_APP_URL);
+
   return (
     <div className="wrapper-bg">
       <div className="home-main">
         <div className=" home-main-content">
           <div className="home-main-content-left">
             <div className="main-text">
-              Let's help stop the spread of COVID-19 within our campus.
+              Let's help stop the spread of COVID-19 within our campus.{url}
             </div>
             <Link to="/login">
               <button className="get-started">Get Started</button>
