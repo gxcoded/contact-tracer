acd05bf96b9cbff8ca5ce10574e39aa7f4c69b10
gilgucela <82348697+gilgucela@users.noreply.github.com>
2022-10-29 22:05:58 +0800

    last

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -11,7 +11,7 @@ const HomePage = () => {
         <div className=" home-main-content">
           <div className="home-main-content-left">
             <div className="main-text">
-              Let's help stop the spread of COVID-19 within our campus.{url}
+              Let's help stop the spread of COVID-19 within our campus.
             </div>
             <Link to="/login">
               <button className="get-started">Get Started</button>
