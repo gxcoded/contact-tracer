5f2a682bfdcdc9fafcfe2893d234d99074f902ea
gilgucela <82348697+gilgucela@users.noreply.github.com>
2022-11-29 14:26:41 +0800

    hello

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -159,7 +159,7 @@ const Student = ({ vaxStatsList, genderList }) => {
                         }}
                         className="side-button"
                       >
-                        <i className="fas fa-vector-square me-3"></i>Scanner1
+                        <i className="fas fa-vector-square me-3"></i>Scanner2
                       </div>
                     </li>
                     <li className="list-group-item px-4 border-0">
